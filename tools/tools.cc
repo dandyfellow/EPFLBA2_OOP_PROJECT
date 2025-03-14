9ffecdebd9ec485907c22f08d923363fc27b118e
DarkAdam123 <coste.adam1806@gmail.com>
2025-03-14 09:30:32 +0100

    include de tools

---
+++ tools/tools.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+#include 'chaine.h'
+#include 'mobile.h' 
+#include 'jeu.h'
+
+using namespace std;
+
+\\struct S2d {double x=0.; double y=0.;};
+\\typedef arary <double,2> S2d;
+
