45cc0c87deed781111a6ee831625f82a03a74e42
Max Wei Ciriani <maxwciriani@gmail.com>
2025-03-29 15:04:30 +0100

    ajouter les nv tests

--- mobile.cc
+++ mobile.cc
@@ -150,13 +150,12 @@ bool lecture_f(istringstream& data) {
         Cercle c_test({new_x, new_y}, rayon);
 
         if (!Cercle::inclusion(arene, c_test)) {
-            cout << "INCLUSION" << endl;
+            //cout << "INCLUSION" << endl;
             v = v.reflechis({position.x, position.y});
             angle = v.get_angle();
             new_x = position.x - i * deplacement * cos(angle);
             new_y = position.y - i * deplacement * sin(angle);
         }
-
         //cout << "position.x " << position.x << " position.y " << position.y << " angle: " << angle << endl;
 
         f->ajouter_element({new_x, new_y});
